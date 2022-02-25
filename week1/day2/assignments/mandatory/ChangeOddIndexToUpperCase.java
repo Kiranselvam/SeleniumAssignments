c60732cafba290c31eb0579141123fc427c153d5
Kiranselvam <100064497+Kiranselvam@users.noreply.github.com>
2022-02-25 16:12:19 +0530

    Update ChangeOddIndexToUpperCase.java

--- week1/day2/assignments/mandatory/ChangeOddIndexToUpperCase.java
+++ week1/day2/assignments/mandatory/ChangeOddIndexToUpperCase.java
@@ -1,22 +1,20 @@
 package week1.day2.assignments.mandatory;
 
-public class ChangeOddIndexToUpperCase {
-	
-	
-	/*
-	 Pseudo Code
-	 
-	 * Declare String Input as Follow
-	  
-	 * String test = "changeme";
-	 
-	 * a) Convert the String to character array
-	 
-	 * b) Traverse through each character (using loop)
-	 
-	 * c)find the odd index within the loop (use mod operator)
-	 
-	 * d)within the loop, change the character to uppercase, if the index is odd else don't change
-	  
-	 */
+public class PrintingStringInAlternativeCase {
+    public static void main(String s[])
+    {
+        String testString = "TESTSTRING";
+        String output = "";
+        for (int i = 0; i < testString.length(); i++) {
+            if(i%2 == 0)
+            {
+                output += Character.toUpperCase(testString.toCharArray()[i]);
+            }else
+            {
+                output += Character.toLowerCase(testString.toCharArray()[i]);
+            }
+        }
+
+        System.out.println("Newly generated string is as follow: "+ output);
+    }
 }
