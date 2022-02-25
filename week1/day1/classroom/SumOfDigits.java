1cb0deb46a8beee58e58d4599189e7a698a6506f
Kiranselvam <100064497+Kiranselvam@users.noreply.github.com>
2022-02-25 16:37:29 +0530

    Update SumOfDigits.java

--- week1/day1/classroom/SumOfDigits.java
+++ week1/day1/classroom/SumOfDigits.java
@@ -2,44 +2,21 @@ package week1.day1.classroom;
 
 public class SumOfDigits {
 
-	/*
-	 * Goal: Find the sum of digits of a given number
-	 * 
-	 * input: 123
-	 * output: 1+2+3 = 6
-	 * 
-	 * Shortcuts:
-	 * 1) Print : type: syso, followed by: ctrl + space + enter
-	 * 2) To create a 'while' loop: type 'while', followed by ctrl + space + enter
-	 *   
-	 * What are my learnings from this code?
-	 * 1)
-	 * 2)
-	 * 3) 
-	 * 
-	 */
+	public class SumofDigits {
 
 	public static void main(String[] args) {
-
-		// Declare your input number (int)
-
-		// Initialize an integer variable by name: sum
-
-		// Use loop to calculate the sum: use while loop to set condition until the number goes greater than 0
-
-		// Within loop: get the remainder when done by 10 -> Tip: use mod %
-
-			// Print the remainder to confirm
-	
-			// Within loop: add that remainder to the sum
-	
-			// Print the sum to confirm
-	
-			// Within loop: get quotient of that number (tip: quotient should be same variable of loop condition)
-	
-			// Print the quotient to confirm
+		int num = 123;
+		int sum=0;
+		for(int i=0;i<num;) {
+			int rem = num%10;
+			System.out.println("Reminder = "+rem);
+			sum = sum+rem;
+			System.out.println("sum ="+sum);
+			num = num/10;
+			
+		}
 		
-		// Outside the loop: print the final sum
+		System.out.println("Total sum="+ sum);
+		
 	}
-
 }
