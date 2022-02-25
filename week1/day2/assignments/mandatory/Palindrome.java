b13275808f8214292fc5a2740808a44e9558338b
Kiranselvam <100064497+Kiranselvam@users.noreply.github.com>
2022-02-25 16:14:03 +0530

    Update Palindrome.java

--- week1/day2/assignments/mandatory/Palindrome.java
+++ week1/day2/assignments/mandatory/Palindrome.java
@@ -1,19 +1,20 @@
 package week1.day2.assignments.mandatory;
 
-public class Palindrome {
-	//Build a logic to find the given string is palindrome or not
-     
-	/*
-	 * Pseudo Code
-	
-	 * a) Declare A String value as"madam"
-	 
-	 * b) Declare another String rev value as ""
-	 * c) Iterate over the String in reverse order
-	 * d) Add the char into rev
-	 * e) Compare the original String with the reversed String, if it is same then print palinDrome 
-	 
-	 * Hint: Use .equals or .equalsIgnoreCase when you compare a String 
-	 */
+	 public static void main(String[] args) {
 
+    String str = "Radar", reverseStr = "";
+    
+    int strLength = str.length();
+
+    for (int i = (strLength - 1); i >=0; --i) {
+      reverseStr = reverseStr + str.charAt(i);
+    }
+
+    if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+      System.out.println(str + " is a Palindrome String.");
+    }
+    else {
+      System.out.println(str + " is not a Palindrome String.");
+    }
+  }
 }
