cacf962fb510d717be3fdaf95e913a9e021fb9bc
Kiranselvam <100064497+Kiranselvam@users.noreply.github.com>
2022-03-04 20:35:11 +0530

    Update CreateContact.java

--- week2/day2.assignments/CreateContact.java
+++ week2/day2.assignments/CreateContact.java
@@ -1,54 +1,83 @@
-package week2.day2.Assignments;
+package Week2.day1;
+import java.time.Duration;
 
-public class CreateContact {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AssignmentCreateContact {
 	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
 		
-		/*
-		 * //Pseudo Code
-		 * 
-		 * 1. Launch URL "http://leaftaps.com/opentaps/control/login"
-		 * 
-		 * 2. Enter UserName and Password Using Id Locator
-		 * 
-		 * 3. Click on Login Button using Class Locator
-		 * 
-		 * 4. Click on CRM/SFA Link
-		 * 
-		 * 5. Click on contacts Button
-		 * 
-		 * 6. Click on Create Contact
-		 *  
-		 * 7. Enter FirstName Field Using id Locator
-		 * 
-		 * 8. Enter LastName Field Using id Locator
-		 * 
-		 * 9. Enter FirstName(Local) Field Using id Locator
-		 * 
-		 * 10. Enter LastName(Local) Field Using id Locator
-		 * 
-		 * 11. Enter Department Field Using any Locator of Your Choice
-		 * 
-		 * 12. Enter Description Field Using any Locator of your choice 
-		 * 
-		 * 13. Enter your email in the E-mail address Field using the locator of your choice
-		 * 
-		 * 14. Select State/Province as NewYork Using Visible Text
-		 * 
-		 * 15. Click on Create Contact
-		 * 
-		 * 16. Click on edit button 
-		 * 
-		 * 17. Clear the Description Field using .clear
-		 * 
-		 * 18. Fill ImportantNote Field with Any text
-		 * 
-		 * 19. Click on update button using Xpath locator
-		 * 
-		 * 20. Get the Title of Resulting Page.
-
-         */
-		
-	}
-
+		 ChromeDriver driver=new ChromeDriver();
+		 driver.get("http://leaftaps.com/opentaps/control/login");
+		 driver.manage().window().maximize();
+		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		 
+	//Enter UserName and Password Using Id Locator
+		 WebElement elementUsername = driver.findElement(By.id("username"));
+			elementUsername.sendKeys("Demosalesmanager");
+			WebElement elementPassword = driver.findElement(By.id("password"));
+			elementPassword.sendKeys("crmsfa");
+  //Click submit using class
+			WebElement elementLoginButton = driver.findElement(By.className("decorativeSubmit"));
+			elementLoginButton.click();
+			WebElement elementCRMSFA = driver.findElement(By.linkText("CRM/SFA"));
+			elementCRMSFA.click();
+	//Click on Contacts & Create Contacts using Partial Text based xpath
+			WebElement elementContacts =driver.findElement(By.xpath("//a[contains(text(),'Contacts')]"));
+			elementContacts.click();	
+			
+			WebElement elementCreateContacts =driver.findElement(By.xpath("//a[contains(text(),'Create')]"));
+			elementCreateContacts.click();	
+	//Enter FirstName and LastName Field Phonenumber Using id Locator		
+			 WebElement elementFirstname = driver.findElement(By.id("firstNameField"));
+			 elementFirstname.sendKeys("Saravanan");
+				WebElement elementLastName = driver.findElement(By.id("lastNameField"));
+				elementLastName.sendKeys("Prakasam");
+				
+				
+				
+	//Enter FirstName(Local) & LastName(Local) Field Using id Locator
+				WebElement elementLocalFirstname = driver.findElement(By.id("createContactForm_firstNameLocal"));
+				elementLocalFirstname.sendKeys("Saravanan");
+					WebElement elementLocalLastName = driver.findElement(By.id("createContactForm_lastNameLocal"));
+					elementLocalLastName.sendKeys("Prakasam");
+					
+	//Enter Department and Description Field Using Locator
+					WebElement elementDepartment = driver.findElement(By.name("departmentName"));
+					elementDepartment.sendKeys("Software Testing");
+					
+					WebElement elementDescription=driver.findElement(By.id("createContactForm_description"));
+					elementDescription.sendKeys ("Software Testing-Associate Test Lead");
+					
+	//Enter E-mail address  Field Using Locator
+					WebElement elementEmail = driver.findElement(By.id("createContactForm_primaryEmail"));
+					elementEmail.sendKeys("saravananbtech10.prakasam@gmail.com");
+					
+   //Select State/Province as NewYork Using Visible Text
+					WebElement elementDropdown=driver.findElement(By.id("createContactForm_generalStateProvinceGeoId"));
+				 	Select dropdownvalue=new Select(elementDropdown);
+							 	dropdownvalue.selectByVisibleText("New York");
+   // Partial attribute based xpath
+			WebElement elementCreateContact =driver.findElement(By.xpath("//input[contains(@name,'submitButton')]"));
+								elementCreateContact.click();	
+								
+								WebElement elementEditContact =driver.findElement(By.xpath("//a[@class = 'subMenuButton']"));
+								elementEditContact.click();
+	 //Clear Description
+								WebElement elementClearDescription=driver.findElement(By.id("updateContactForm_description"));
+								elementClearDescription.clear();
+	//Enter ImportantNote Field using Id locator
+								WebElement elementImportantNote = driver.findElement(By.id("updateContactForm_importantNote"));
+								elementImportantNote.sendKeys("Learning Selenium Automation in TestLeaf");	
+	//Click Update Button
+				WebElement elementUpdateContact =driver.findElement(By.xpath("//input[@class = 'smallSubmit']"));
+				elementUpdateContact.click();			
+	//Get Title oF Resulting Page
+						System.out.println("Page Title is:  " + driver.getTitle());
+}
 }
